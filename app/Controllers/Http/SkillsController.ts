625c2296d67385cc1e294077617646427c6d2912
jcleston <janes.cleston.silva@gmail.com>
2022-09-01 11:01:17 -0300

    Criando o método show para Skill

--- app/Controllers/Http/SkillsController.ts
+++ app/Controllers/Http/SkillsController.ts
@@ -1,13 +1,21 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 import Skill from 'App/Models/Skill'
 
 export default class SkillsController {
   public async index() {
-    const users = await Skill.query()
+    const skills = await Skill.query()
 
     return {
-      data: users,
+      data: skills,
+    }
+  }
+
+  public async show({ params }: HttpContextContract) {
+    const skill = await Skill.findOrFail(params.id)
+
+    return {
+      data: skill,
     }
   }
 }
